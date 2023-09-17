3d0003ede94e38ba73f6eea3137cbbac61a7c35c
Kyu Lee <kwooler@gmail.com>
2023-09-17 16:19:03 -0700

    CS120A59

--- main.cpp
+++ main.cpp
@@ -4,25 +4,18 @@ using namespace std;
 
 int main()
 {
-	int N; // the number of students;
-	int M; // the number of scores;
+	/* ****************************
+	// Complete this code.
+	*************************** */
+	int prev, current;
+	int flag = 1;
 
-	cout << "Enter the number of students: " ;
-	cin >> N;
-	cout << "Enter the number of scores for each student: " ;
-	cin >> M;
-	
-	for(int i=0; i<N; i++)
+	cin >> current;
+	while ((flag == 1) or (prev > current))
 	{
-		for (int j=0; j<M; j++)
-		{
-			/* ****************************
-			// Complete this code.
-			*************************** */
-			
-		}
+		flag = 0;
+		cout << current << "\t";
+		prev = current;
+		cin >> current;
 	}
-
-	
-	
 }
