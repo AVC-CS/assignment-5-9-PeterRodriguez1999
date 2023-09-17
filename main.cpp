ed226745b3deddf20096f49a89a9a7a9f553d007
Kyu Lee <kwooler@gmail.com>
2023-09-17 16:52:45 -0700

    CS120A59

--- main.cpp
+++ main.cpp
@@ -4,18 +4,8 @@ using namespace std;
 
 int main()
 {
+	int prev, current;
 	/* ****************************
 	// Complete this code.
 	*************************** */
-	int prev, current;
-	int flag = 1;
-
-	cin >> current;
-	while ((flag == 1) or (prev > current))
-	{
-		flag = 0;
-		cout << current << "\t";
-		prev = current;
-		cin >> current;
-	}
 }
